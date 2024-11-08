cab1a882e179368be9825e6cec4b14f99acf2131
Alexandria Cox <alexacox.atc@gmail.com>
2024-11-08 00:04:36 -0500

    feat: :sparkles: Add difficulty functionality

--- js/script.js
+++ js/script.js
@@ -1,8 +1,74 @@
+//TO DO in order
+// once done we will circle back to:
+// deletion single card
+// editing
+// filtered mode i.e. show all hard cards
+// Show difficulty in the buttons
+
+//remember to add:
+// flip animation
+// feedback messages to let users know action was successful. like deleting, editing, addition
+
 // VARIABLES //
 let card_deck = JSON.parse(localStorage.getItem('flashcardDeck')) || []; //get saved deck or a null array
 let currentCardIndex = 0;
 const displayInfoFront = document.querySelector('#cardFront');
 const displayInfoBack = document.querySelector('#cardBack');
+const createFront = document.getElementById('createFront');
+const createBack = document.getElementById('createBack');
+const successMessage = document.getElementById('successMessage');
+const btnEasy = document.getElementById('btnEasy');
+const btnMedium = document.getElementById('btnMedium');
+const btnHard = document.getElementById('btnHard');
+
+// ON LOAD //
+window.onload = function () {
+  const savedDeck = localStorage.getItem('flashcardDeck');
+  if (savedDeck) {
+    card_deck = JSON.parse(savedDeck);
+    card_deck = weightedShuffle(card_deck);
+    displayCard();
+  }
+};
+
+// lOCAL STORAGE //
+function updateLocalStorage() {
+  localStorage.setItem('flashcardDeck', JSON.stringify(card_deck));
+}
+
+function clearLocalStorage() {
+  localStorage.removeItem('flashcardDeck');
+  card_deck.length = 0;
+  displayCard();
+}
+
+// SHUFFLE //
+function weightedShuffle(deck) {
+  const weightedDeck = [];
+
+  // Creating weights //
+  deck.forEach((card) => {
+    let weight = 1;
+    if (card.infoDifficulty === 'medium') {
+      weight = 2;
+    } else if (card.infoDifficulty === 'hard') {
+      weight = 3;
+    }
+
+    // Push the card to the deck to match weight //
+    for (let i = 0; i < weight; i++) {
+      weightedDeck.push(card);
+    }
+  });
+
+  // Shuffle //
+  for (let i = weightedDeck.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [weightedDeck[i], weightedDeck[j]] = [weightedDeck[j], weightedDeck[i]];
+  }
+
+  return weightedDeck;
+}
 
 // DISPLAY CARD //
 function displayCard() {
@@ -15,6 +81,8 @@ function displayCard() {
   const currentCard = card_deck[currentCardIndex];
   displayInfoFront.innerHTML = currentCard.infoFront;
   displayInfoBack.innerHTML = currentCard.infoBack;
+
+  checkDifficulty(currentCard);
 }
 
 // CREATE A CARD //
@@ -26,9 +94,55 @@ function createCard() {
   };
   card_deck.push(cardInfo); //adds to the array
   updateLocalStorage(); //updates local storage
-  createFront.value = ' '; //clears the textareas for additional input
-  createBack.value = ' ';
-  displayCard(); //calls the display card function to display the newly added card
+
+  // clear textareas //
+  createFront.value = '';
+  createBack.value = '';
+
+  // success message //
+  successMessage.classList.add('d-block');
+  successMessage.classList.remove('d-none');
+
+  // HIDE SUCCESS //
+  setTimeout(() => {
+    successMessage.classList.add('d-none');
+    successMessage.classList.remove('d-block');
+  }, 2000);
+}
+
+// HIDE SUCCESS //
+function handleTyping() {
+  if (successMessage.classList.contains('d-block')) {
+    successMessage.classList.add('d-none');
+    successMessage.classList.remove('d-block');
+  }
+}
+createFront.addEventListener('input', handleTyping);
+createBack.addEventListener('input', handleTyping);
+
+// DIFFICULTY //
+function checkDifficulty(card) {
+  // RESET BUTTONS //
+  btnEasy.classList.remove('active');
+  btnMedium.classList.remove('active');
+  btnHard.classList.remove('active');
+
+  // SET BUTTON //
+  if (card.infoDifficulty === 'easy') {
+    btnEasy.classList.add('active');
+  } else if (card.infoDifficulty === 'medium') {
+    btnMedium.classList.add('active');
+  } else if (card.infoDifficulty === 'hard') {
+    btnHard.classList.add('active');
+  }
+}
+
+function setDifficulty(difficulty) {
+  if (card_deck.length === 0) return;
+  card_deck[currentCardIndex].infoDifficulty = difficulty;
+  updateLocalStorage();
+  displayCard();
+  checkDifficulty(card_deck[currentCardIndex]);
 }
 
 // CHANGE CARD //
